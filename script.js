bbcba32cb40a03d95912a8f8724b7928c2bc29b8
til2to <padwusup@gmail.com>
2023-01-10 12:20:48 +0000

    Fix linters errors

--- script.js
+++ script.js
@@ -1,77 +1,70 @@
-const addBookBtn = document.querySelector('.add')
-const newBooks =document.querySelector('.new-books-container')
-let title = document.getElementById('title')
-const form = document.querySelector('.form')
-let author=  document.getElementById('author')
-const list = document.querySelector('.wishList')
+const newBooks = document.querySelector('.new-books-container');
+const title = document.getElementById('title');
+const form = document.querySelector('.form');
+const author = document.getElementById('author');
 
-
-let local_storage =  JSON.parse(window.localStorage.getItem('allBooks'))
-if(local_storage === null || local_storage === undefined || local_storage.length === 0 ){
-	local_storage = []
+let localStorage = JSON.parse(window.localStorage.getItem('allBooks'));
+if (localStorage === null || localStorage === undefined || localStorage.length === 0) {
+  localStorage = [];
 }
 
-let allBooks = local_storage
+let allBooks = localStorage;
 
-/* render each book */ 
+/* render each book */
 allBooks.forEach((book, index) => {
-	const displayBook = `
-	<div class="book-container">
-		<p class="book-title">title: ${book.title}</p>
-		<p class="book-author">Author: ${book.author}</p>
-		<button class="remove" id=${index}>Remove</button>
-	</div>
-	`
-	newBooks.innerHTML += displayBook;
-})
+  const displayBook = `
+  <div class="book-container">
+    <p class="book-title">title: ${book.title}</p>
+    <p class="book-author">Author: ${book.author}</p>
+    <button class="remove" id=${index}>Remove</button>
+  </div>
+  `;
+  newBooks.innerHTML += displayBook;
+});
 
-/*compare the books*/ 
-const bookExist = (existiingTitle, newTitle) => {
-	return JSON.stringify(existiingTitle) === JSON.stringify(newTitle)
-}
+/* compare the books */
+const bookExist = (existiingTitle,
+  newTitle) => JSON.stringify(existiingTitle) === JSON.stringify(newTitle);
 
-/* add book */ 
+/* add book */
 const addBook = () => {
-	let newBook = {
-		title: title.value,
-		author: author.value,
-	}
-	/* check if book exist */ 
-	let exist = false;
-	local_storage.forEach((book) => {
-		if(bookExist(book.title, title.value)){
-			exist = true
-			console.log('book exist')
-		}
-	});
+  const newBook = {
+    title: title.value,
+    author: author.value,
+  };
+  /* check if book exist */
+  let exist = false;
+  localStorage.forEach((book) => {
+    if (bookExist(book.title, title.value)) {
+      exist = true;
+    }
+  });
 
-	/* Dont add if book exist */ 
-	if (exist) return 
+  /* Dont add if book exist */
+  if (exist) return;
 
-	/* add book if it doesn't exist already */ 
-	else {
-		allBooks.unshift(newBook)
-		window.localStorage.setItem('allBooks', JSON.stringify(local_storage))
-		window.location.reload();
-	}
+  /* add book if it doesn't exist already */
+  allBooks.unshift(newBook);
+  window.localStorage.setItem('allBooks', JSON.stringify(localStorage));
+  window.location.reload();
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBook();
+});
+
+/* remove book */
+function remove(buttonId) {
+  allBooks = localStorage.filter((book, index) => index !== buttonId);
+  window.localStorage.setItem('allBooks', JSON.stringify(allBooks));
+  window.location.reload();
 }
 
-form.addEventListener('submit', (e)=>{
-	e.preventDefault()
-	addBook()		
-})
-
-/* remove book */ 
-function remove (buttonId) {
-	allBooks = local_storage.filter((book, index) => index != buttonId)
-	window.localStorage.setItem('allBooks', JSON.stringify(allBooks))
-	window.location.reload();
-}
-
-const bookBtns = document.querySelectorAll(".remove");
+const bookBtns = document.querySelectorAll('.remove');
 bookBtns.forEach((bookBtn) => {
-	bookBtn.addEventListener('click', (e)=>{
-		buttonId = e.target.getAttribute('id')
-		remove(buttonId)
-	})
-})
+  bookBtn.addEventListener('click', (e) => {
+    const buttonId = parseInt(e.target.getAttribute('id'), 10);
+    remove(buttonId);
+  });
+});
