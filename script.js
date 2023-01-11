1c80582c4735aadf633a0ee20456dc12a893e746
til2to <padwusup@gmail.com>
2023-01-11 10:27:06 +0000

    Make data persistent with local storage

--- script.js
+++ script.js
@@ -4,9 +4,6 @@ import Book from './book.js'
 class BookList {
   constructor() {
     this.storage = JSON.parse(window.localStorage.getItem('allBooks')) || [];
-    // if (this.localStorage === null || this.localStorage === undefined || this.localStorage.length === 0) {
-    //   this.ocalStorage = [];
-    // }
     this.allBooks = this.storage;
     this.title = document.getElementById('title');
     this.form = document.querySelector('.form');
@@ -21,14 +18,34 @@ class BookList {
       e.preventDefault();
       this.addBook();
     });
+
+    console.log(this.allBooks)
+    this.allBooks.forEach((book, index) => {
+      const displayBook = `
+      <div class="book-container">
+        <p class="book-title">title: ${book.title}</p>
+        <p class="book-author">Author: ${book.author}</p>
+        <button class="remove" id=${index}>Remove</button>
+      </div>
+      `;
+      this.newBooks.innerHTML += displayBook
+    });
+
+    const bookBtns = document.querySelectorAll('.remove');
+    bookBtns.forEach((bookBtn) => {
+      bookBtn.addEventListener('click', (e) => {
+        const buttonId = parseInt(e.target.getAttribute('id'), 10);
+        this.remove(buttonId);
+      });
+    });
   }
-  
+
   /* add book */
   addBook(title, author){
     const newBook = new Book(this.title.value, this.author.value)
     /* check if book exist */
     let exist = false;
-    this.allBooks.forEach((book) => {
+    this.storage.forEach((book) => {
       if (this.bookExist(book.title, this.title.value)) {
         exist = true;
       }
@@ -39,40 +56,15 @@ class BookList {
   
     /* add book if it doesn't exist already */
     this.allBooks.unshift({title: newBook.title, author: newBook.author});
-    window.localStorage.setItem('allBooks', JSON.stringify(this.allBooks));
-    this.displayBooks()
+    window.localStorage.setItem('allBooks', JSON.stringify(this.storage));
+    window.location.reload()
   }
 
   /* remove book */
   remove(buttonId) {
     this.allBooks = this.allBooks.filter((book, index) => index !== buttonId);
-    this.displayBooks()
     window.localStorage.setItem('allBooks', JSON.stringify(this.allBooks));
-  }
-
-  displayBooks() {
-    this.newBooks.innerHTML = '';
-
-    /* render each book */
-    this.allBooks.forEach((book, index) => {
-      const newElement = document.createElement('li');
-      newElement.innerHTML = `
-      <div class="book-container">
-        <p class="book-title">title: ${book.title}</p>
-        <p class="book-author">Author: ${book.author}</p>
-        <button class="remove" id=${index}>Remove</button>
-      </div>
-      `;
-      this.newBooks.appendChild(newElement);
-    });
-
-    const bookBtns = document.querySelectorAll('.remove');
-    bookBtns.forEach((bookBtn) => {
-      bookBtn.addEventListener('click', (e) => {
-        const buttonId = parseInt(e.target.getAttribute('id'), 10);
-        this.remove(buttonId);
-      });
-    });
+    window.location.reload();
   }
 }
 
